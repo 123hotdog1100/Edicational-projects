8833e1a126ac7607f232d9ca730dfe9616411ed6
123hotdog1100 <123hotdog1100@gmail.com>
2022-04-10 02:25:09 +0100

    Comments Cleanup

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -5,5 +5,5 @@ for n in range(100):
     if (n %  3 == 00):#Checks to see if the moudle returns 0 for the iteration and 3
         output = output +  "Buzz"#Appends "Buzz" to the output string
     else:
-        output = n#"Sets the output to the itteration if nothing no test was passed"
+        output = n#Sets the output to the itteration if nothing no test was passed
     print(output)#Outputs the variable output
